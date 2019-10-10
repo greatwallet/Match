252c2938f66bd46a44ea93c645a901db63e8082d
greatwallet <cxt_tsinghua@126.com>
2019-10-10 22:20:51 +0800

    Update 1: rough drafts

---
+++ Expr.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Match
+{
+    // Calculate the result from string expression
+    class Expr
+    {
+        const int N_OPTR = 5;
+        // Priority Matrix for operators[Top, Current]
+        readonly char[,] pri = new char[N_OPTR, N_OPTR] {
+        /* Current Operator*/
+        /*  +       -       *       \/      \0      */
+/*  T    + */   { '>',    '>',    '<',    '<',    '>' },
+/*  o    - */   { '>',    '>',    '<',    '<',    '>' },
+/*  p    * */   { '>',    '>',    '>',    '>',    '>' },
+/*      \/ */   { '>',    '>',    '>',    '>',    '>' },
+/*     \0  */   { '<',    '<',    '<',    '<',    '=' }
+            };
+
+        // check if a char is digit
+        private bool isdigit(char c)
+        {
+            return c - '0' >= 0 && c - '0' <= 9;
+        }
+
+        // get the index of operators in the "pri" Matrix
+        private int IndexOfOprd(char op)
+        {
+            switch (op)
+            {
+                case '+':return 0;
+                case '-':return 1;
+                case '*': return 2;
+                case '/':return 3;
+                case '\0':return 4;
+                default:throw new Exception("Error in IndexOfOprd");
+            }
+        }
+
+        // Compare the priority of 2 operators
+        private char orderBetween(char top,char cur)
+        {
+            return pri[IndexOfOprd(top), IndexOfOprd(cur)];
+        }
+
+        // read single or multiple digit - number from the expr, 
+        // and increase the index to the next of the end of the  digits
+        private double readNumber(string expr,ref int idx)
+        {
+            double num = 0; // Supposed to be an integer stored in double precision format
+            for (; idx < expr.Length && isdigit(expr[idx]); idx++) 
+            {
+                num *= 10;
+                num += Convert.ToDouble(expr[idx] - '0');
+            }
+            return num;
+        }
+        // Get the result of calculation of 2 numbers
+        private double calcu(double pOpnd1, char op, double pOpnd2)
+        {
+            switch (op)
+            {
+                case '+':
+                    return pOpnd1 + pOpnd2;
+                case '-':
+                    return pOpnd1 - pOpnd2;
+                case '*':
+                    return pOpnd1 * pOpnd2;
+                case '/':
+                    return pOpnd1 / pOpnd2;
+                default:
+                    throw new Exception("Error in calcu");
+            }
+        }
+        // To get the result of an normal expr  
+        public double evaluate(string expr)
+        {
+            expr = String.Concat(expr, '\0');
+            // Stacks for operands and operators
+            var opnd = new Stack<double>();
+            var optr = new Stack<char>();
+            // Put in ending mark
+            optr.Push('\0');
+            for (int idx = 0; optr.Count > 0;) 
+            {
+                if (isdigit(expr[idx]))
+                {
+                    double num = readNumber(expr, ref idx);
+                    opnd.Push(num);
+                }
+                else
+                {
+                    switch (orderBetween(optr.Peek(), expr[idx]))
+                    {
+                        // top < current
+                        case '<':
+                            optr.Push(expr[idx]);idx++;
+                            break;
+                        // the expr ends
+                        case '=':
+                            optr.Pop();idx++;
+                            break;
+                        // top > current, time for calculating
+                        case '>':
+                            {
+                                // get the operator
+                                char op = optr.Pop();
+                                // Get 2 operands
+                                double pOpnd2 = opnd.Pop(), pOpnd1 = opnd.Pop();
+                                opnd.Push(calcu(pOpnd1, op, pOpnd2));
+                                break;
+                            }
+                        default:
+                            throw new Exception("Error in evaluate");
+                    }
+                }
+            }
+            // opnd.Count Supposed to be 1;
+            return opnd.Pop();
+        }
+    }
+}
---
+++ SSD.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+// Seven-Segment-Display Digit
+namespace Match
+{
+    // Remove or Place: Attribute of Action and State
+    enum Action
+    {
+        Remove = 0, Place = 1
+    };
+    class SSD
+    {
+        // Binary Coded decimal
+        public byte BCD;
+        
+        /* init function */
+        // No input, the BCD is set to Null.
+        public SSD()
+        {
+            BCD = 0x00;
+        }
+
+        // input = integer
+        public SSD(int d)
+        {
+            BCD = digit2binary(d);
+        }
+
+        // input = character of digit
+        public SSD(char c)
+        {
+            BCD = digit2binary(Convert.ToByte(c - '0'));
+        }
+        // input = BCD of the SSD
+        public SSD(byte bcd)
+        {
+            BCD = bcd;
+        }
+        // Convert digit to BCD
+        private byte digit2binary(int d)
+        {
+            switch(d)
+            {
+                case 0: return 0x3f;
+                case 1: return 0x06;
+                case 2: return 0x5b;
+                case 3: return 0x4f;
+                case 4: return 0x66;
+                case 5: return 0x6d;
+                case 6: return 0x7d;
+                case 7: return 0x07;
+                case 8: return 0x7f;
+                case 9: return 0x6f;
+                default: throw new Exception("Error in digit2Binary");
+            }
+        }
+
+        // Convert BCD to digit
+        private int binary2digit(byte b)
+        {
+            switch (b)
+            {
+                case 0x3f: return 0;
+                case 0x06: return 1;
+                case 0x5b: return 2;
+                case 0x4f: return 3;
+                case 0x66: return 4;
+                case 0x6d: return 5;
+                case 0x7d: return 6;
+                case 0x07: return 7;
+                case 0x7f: return 8;
+                case 0x6f: return 9;
+                default: throw new Exception("Error in binary2digit");
+            }
+        }
+
+        // Get the digit value of current BCD
+        public int getVal()
+        {
+            return binary2digit(BCD);
+        }
+
+        /* re-define operator equal & non-equal */
+        public static bool operator ==(SSD a, SSD b)
+        {
+            return a.BCD == b.BCD;
+        }
+        public static bool operator !=(SSD a, SSD b)
+        {
+            return a.BCD != b.BCD;
+        }
+
+        //Set a bit to zero of the BCD.
+        // 0<=n<=7; means the position of the bit 
+        private byte _setZeroBit(int n)
+        {
+            Debug.Assert(n >= 0 && n <= 7);
+            return Convert.ToByte(BCD & (~(0x01 << n)));
+        }
+
+        //Set a bit to one of the BCD.
+        // 0<=n<=7; means the position of the bit 
+        public byte _setOneBit(int n)
+        {
+            Debug.Assert(n >= 0 && n <= 7);
+            return Convert.ToByte(BCD | (0x01 << n));
+        }
+
+        // expand the SSD by removing or placing ONE match
+        public List<SSD> expand(Action act)
+        {
+            var new_SSD_list = new List<SSD>();
+            // Remove one match
+            if (act == Action.Remove)
+            {
+                for (int idx = 0; idx <= 6; idx++)
+                {
+                    // generate new ssd after removing 
+                    byte bcd_rm = _setZeroBit(idx);
+                    if (bcd_rm == BCD) continue;
+                    SSD ssd_rm = new SSD(bcd_rm);
+                    new_SSD_list.Add(ssd_rm);
+                }
+            }
+            // Place one match
+            else
+            {
+                for (int idx = 0; idx <= 6; idx++)
+                {
+                    // generate new ssd after placing 
+                    byte bcd_pl = _setOneBit(idx);
+                    if (bcd_pl == BCD) continue;
+                    SSD ssd_pl = new SSD(bcd_pl);
+                    new_SSD_list.Add(ssd_pl);
+                }
+            }
+            return new_SSD_list;
+        }
+
+        /* Functions aids to cut branches */
+        
+        // get the "Match Loss" of the digit
+        // public int match_loss()
+    }
+}
